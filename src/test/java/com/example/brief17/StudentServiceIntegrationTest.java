caeccfb2e5868ddfc81dc12b3f98f1a10e3a55fd
corantoine <coralie.antoine@maif.fr>
2025-02-04 13:59:18 +0100

    test: test student insertion in db ok

--- src/test/java/com/example/brief17/StudentServiceIntegrationTest.java
+++ src/test/java/com/example/brief17/StudentServiceIntegrationTest.java
@@ -1,5 +1,8 @@
 package com.example.brief17;
 
+import org.assertj.core.api.Assert;
+import org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,10 +17,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Optional;
 
 // TODO: Ajouter les tags nécessaires pour charger H2, charger le profil de test et importer le StudentService
+
+@DataJpaTest
+@Import(StudentService.class) // Importer le StudentService
+@ActiveProfiles("test")
 class StudentServiceIntegrationTest {
+@Autowired
+private StudentService studentService;
 
     @Test
     void shouldSaveAndRetrieveStudent() {
-        // TODO: Implémenter le test d'intégration, insérer un Student en base de données et le récupérrer
+        // TODO: Implémenter le test d'intégration, insérer un Student en base de données et le récupérer
+        Student student1 = new Student();
+        student1.setName("a");
+        student1.setAddress("b");
+
+        Student resultStudent = studentService.saveStudent(student1);
+
+        Optional<Student> savedStudent1 = studentService.findStudentById(resultStudent.getId());
+
+        assertTrue(savedStudent1.isPresent());
+        assertThat(savedStudent1.get().getName()).isEqualTo("a");
+        assertThat(savedStudent1.get().getAddress()).isEqualTo("b");
+
+
     }
 }
